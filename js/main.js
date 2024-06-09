1ef0603f202a3020537242009655ededab01cce1
tanvirahmedrume <tstanvir371806@gmail.com>
2024-06-09 15:57:23 +0600

    mobile NavBar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-var threeDots = document.getElementById('threeDots');
-var crossDot = document.getElementById('crossDot');
-var dropdown = document.querySelector('.dropdown');
+let navClick = document.getElementById("navClick");
+let navClose = document.getElementById("navClose");
+let dropdown = document.querySelector(".dropdown-content");
+
+navClick.addEventListener("click", function(event) {
+  navClose.classList.remove("hidden");
+  dropdown.classList.remove("hidden");
+  navClick.classList.add("hidden");
+});
+
+navClose.addEventListener("click", function(event) {
+  navClick.classList.remove("hidden");
+  navClose.classList.add("hidden");
+  dropdown.classList.add("hidden");
+});
